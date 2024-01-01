a6050888b75c113c80185b9e0998c30b8fae9fcb
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2023-12-31 20:15:42 -0500

    created animated sliders for cup pong play icon

--- js/script.js
+++ js/script.js
@@ -13,4 +13,48 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
+    function startPongIconAnimation() {
+        const slider1 = document.querySelector("#isliderOne");
+        const slider2 = document.querySelector("#isliderTwo");
+        // const ball = document.querySelector("#iball")
+
+        slideAnimation(slider1, 0, 1);
+        slideAnimation(slider2, 60, 1);
+
+        function slideAnimation(slider, position, speed) {
+            let y = position;
+            let goingUp = false;
+            let s = speed;
+
+            setInterval(() => {
+                if (y == 0) {
+                    goingUp = false;
+                    y += s;
+                }
+
+                else if (y == 60) {
+                    goingUp = true;
+                    y -= s;
+                }
+
+                else if (goingUp) {
+                    y -= s;
+                }
+
+                else {
+                    y += s;
+                }
+
+                slider.style.top = y + "px";
+
+            }, 20);
+        }
+
+        function ballAnimation() {
+        }
+
+    }
+
+    startPongIconAnimation();
+
 });
